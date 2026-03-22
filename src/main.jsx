b45d7d2a070e900e9bf8464e37cec72d6384584e
Admin <krishanbhakar32@gmail.com>
2026-03-22 17:08:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import NotificationContainer from './components/Notification.jsx'
 import './styles/global.css'
 
@@ -10,8 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
-        <NotificationContainer />
+        {/* BUG FIX: AuthProvider was defined but never wrapped the app */}
+        <AuthProvider>
+          <App />
+          <NotificationContainer />
+        </AuthProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
