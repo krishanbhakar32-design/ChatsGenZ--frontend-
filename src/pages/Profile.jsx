5cb4ebf3055826217042f44dfe54187f75284fd5
Admin <krishanbhakar32@gmail.com>
2026-04-03 22:18:36 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
-import { API_URL } from '../constants.js'
+import { API_URL } from '../siteConfig.js'
 
 const API = API_URL
 
